a6cba79483b965f29f034e51fd04083728ae4af5
spipno <157675920+spipno@users.noreply.github.com>
2025-03-08 19:45:15 -0500

    Add files via upload

---
+++ AncientsOfDivinity/Content/Items/Projectiles/LycansinesSpadeBladeProjectile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AncientsOfDivinity.Content.Items.Projectiles
+{
+    public class LycansinesSpadeBladeProjectile : ModProjectile
+    {
+        public override void SetDefaults()
+        { 
+         
+        
+            Projectile.width = 32;
+            Projectile.height = 32;
+            Projectile.aiStyle = 70;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.penetrate = 5;
+            Projectile.timeLeft = 300;
+            Projectile.light = 0.15f;
+            Projectile.ignoreWater = true;
+            Projectile.tileCollide = false;
+            Projectile.extraUpdates = 0;
+            
+
+        }
+        
+    }
+}
---
+++ AncientsOfDivinity/Content/Items/Weapons/LycansinesSpadeBlade.cs
@@ -0,0 +1,42 @@
+using AncientsOfDivinity.Content.Items.Projectiles;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AncientsOfDivinity.Content.Items.Weapons
+{ 
+	// This is a basic item template.
+	// Please see tModLoader's ExampleMod for every other example:
+	// https://github.com/tModLoader/tModLoader/tree/stable/ExampleMod
+	public class LycansinesSpadeBlade : ModItem
+	{
+		// The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.AncientsOfDivinity.hjson' file.
+		public override void SetDefaults()
+		{
+			Item.damage = 70000;
+			Item.DamageType = DamageClass.Melee;
+			Item.width = 40;
+			Item.height = 40;
+			Item.useTime = 30;
+			Item.useAnimation = 30;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.knockBack = 10;
+			Item.crit = 6;
+			Item.value = Item.buyPrice(silver: 1);
+			Item.rare = ItemRarityID.Blue;
+			Item.UseSound = SoundID.Item1;
+			Item.autoReuse = true;
+			Item.shoot = ModContent.ProjectileType<LycansinesSpadeBladeProjectile>();
+			Item.shootSpeed = 15;
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.DirtBlock, 10);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.Register();
+		}
+
+	}
+}
